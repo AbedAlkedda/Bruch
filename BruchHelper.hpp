42646a7bfeaae1bd0d213efe3951becfbd2624fa
abed <zagdkde@gmail.com>
2021-02-03 21:00:48 +0100

    fixes

--- BruchHelper.hpp
+++ BruchHelper.hpp
@@ -8,7 +8,7 @@ void multiply_Fractions(std::unordered_map<std::string, long int> &args);
 bool hasSameDenominator(const long int &lhs_nenner, const long int &rhs_nenner);
 bool isDenominatorsZero(const long int &lhs_nenner, const long int &rhs_nenner);
 std::unordered_map<std::string, long int> BruchArgs(const Bruch &lhs, const Bruch &rhs);
-std::unordered_map<std::string, long int> BruchArgs(const Bruch &lhs, const std::array<long int, 2> &rhs);
 void add_Fractions(std::unordered_map<std::string, long int> &args, bool has_same_denominator);
 void divide_Fractions(std::unordered_map<std::string, long int> &args, bool has_same_denominator);
+std::unordered_map<std::string, long int> BruchArgs(const Bruch &lhs, const std::array<long int, 2> &rhs);
 std::unordered_map<std::string, bool> getBruchValidation(const long int &lhs_nenner, const long int &rhs_nenner);
